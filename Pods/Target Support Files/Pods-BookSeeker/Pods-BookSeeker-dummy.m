56cc4d59af5d97afa1fd374114f81f1908849ed7
Teobaldo Moura <tmoura@ciandt.com>
2019-09-26 16:57:10 -0300

    Add .gitignore to swift, xcode and cocoapods

--- Pods/Target Support Files/Pods-BookSeeker/Pods-BookSeeker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BookSeeker : NSObject
-@end
-@implementation PodsDummy_Pods_BookSeeker
-@end
--- Pods/Target Support Files/Pods-BookSeekerUITests/Pods-BookSeekerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BookSeekerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_BookSeekerUITests
-@end
