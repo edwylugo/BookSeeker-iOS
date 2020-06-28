36a0dfa4238cb904e1d35493edef35f81d8b65cd
Edwy Lugo <edwylugo@gmail.com>
2020-06-28 12:28:50 -0300

    - Implementação CheckInternet - Implementação Observable para ViewModel - Implementação/Configuração Coordinator Protocol e NavigationControlller

---
+++ BookSeeker/CheckInternet.swift
@@ -0,0 +1,9 @@
+//
+//  CheckInternet.swift
+//  BookSeeker
+//
+//  Created by Edwy Lugo on 28/06/20.
+//  Copyright © 2020 CIT. All rights reserved.
+//
+
+import Foundation
---
+++ BookSeeker/Coordinator/NavigationController.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationController.swift
+//  BookSeeker
+//
+//  Created by Edwy Lugo on 28/06/20.
+//  Copyright © 2020 CIT. All rights reserved.
+//
+
+import Foundation
